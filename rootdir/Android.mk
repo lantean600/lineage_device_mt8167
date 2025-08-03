72fbdd7f29a2c13a8abba965c98a9e204c9de22f
XingKongTanSuo <3329989998@qq.com>
2025-08-04 05:57:31 +0800

    Add files via upload

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -31,3 +31,11 @@ LOCAL_SRC_FILES    := etc/ueventd.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE       := ueventd.mt8167.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/ueventd.rc
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
